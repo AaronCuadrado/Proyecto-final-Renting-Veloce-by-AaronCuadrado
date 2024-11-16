af7ca058932b9dea7dbf29a0ae1682512474b232
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-11-16 16:33:31 +0000

    backend finish

--- src/app.py
+++ src/app.py
@@ -1,72 +1,68 @@
-"""
-This module takes care of starting the API Server, Loading the DB and Adding the endpoints
-"""
 import os
-from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import Flask
+from flask_session import Session
 from flask_migrate import Migrate
-from flask_swagger import swagger
-from api.utils import APIException, generate_sitemap
 from api.models import db
-from api.routes import api
 from api.admin import setup_admin
 from api.commands import setup_commands
+from api.webhook import webhook
+from api.users import users
+from api.vehicles import vehicles
+from api.transaction import transaction
+from api.reservations import reservations
+from api.auth import auth
+from flask_cors import CORS
 
-# from models import Person
 
-ENV = "development" if os.getenv("FLASK_DEBUG") == "1" else "production"
-static_file_dir = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), '../public/')
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
-# database condiguration
+CORS(app, supports_credentials=True)
+
+# Configuración de la base de datos 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
-        "postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
 else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///renting.db"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-MIGRATE = Migrate(app, db, compare_type=True)
-db.init_app(app)
 
-# add the admin
+# Inicialización de la base de datos y migraciones
+db.init_app(app)
+MIGRATE = Migrate(app, db)
+
+# Configurar el admin
 setup_admin(app)
 
-# add the admin
+# Configurar los comandos
 setup_commands(app)
 
-# Add all endpoints form the API with a "api" prefix
-app.register_blueprint(api, url_prefix='/api')
-
-# Handle/serialize errors like a JSON object
-
-
-@app.errorhandler(APIException)
-def handle_invalid_usage(error):
-    return jsonify(error.to_dict()), error.status_code
-
-# generate sitemap with all your endpoints
+# Registrar el blueprint para las rutas
+app.register_blueprint(users, url_prefix='/api')
+app.register_blueprint(vehicles, url_prefix='/api')
+app.register_blueprint(reservations, url_prefix='/api')
+app.register_blueprint(webhook, url_prefix='/api')
+app.register_blueprint(auth, url_prefix='/api')
+app.register_blueprint(transaction, url_prefix='/api')
 
 
+# Ruta principal
 @app.route('/')
-def sitemap():
-    if ENV == "development":
-        return generate_sitemap(app)
-    return send_from_directory(static_file_dir, 'index.html')
+def home():
+    return "Bienvenido al sistema de renting de vehiculos"
 
-# any other endpoint will try to serve it like a static file
-@app.route('/<path:path>', methods=['GET'])
-def serve_any_other_file(path):
-    if not os.path.isfile(os.path.join(static_file_dir, path)):
-        path = 'index.html'
-    response = send_from_directory(static_file_dir, path)
-    response.cache_control.max_age = 0  # avoid cache memory
-    return response
+# Configurar la clave secreta y el tipo de sesión
+app.config['SECRET_KEY'] = 'ClaveTopSecret'  # Clave secreta 
+app.config['SESSION_TYPE'] = 'filesystem'  # Usar sistema de archivos para manejar sesiones
+app.config['SESSION_PERMANENT'] = False #que la sesion se cierre con el navegador
+app.config['SESSION_COOKIE_HTTPONLY'] = True  # Asegúrate de que la cookie es segura
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'  # Permite solicitudes cross-site
+app.config['SESSION_COOKIE_SECURE'] = True  # Requiere HTTPS
 
 
-# this only runs if `$ python src/main.py` is executed
+Session(app)
+
+# Ejecutar la aplicación si se ejecuta directamente
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=3001, debug=True)
