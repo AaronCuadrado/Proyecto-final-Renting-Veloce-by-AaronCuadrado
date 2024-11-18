f010d0d15c79d3bdef0a72f18d8e91e958934216
Aaron Cuadrado <163054420+AaronCuadrado@users.noreply.github.com>
2024-11-18 01:01:44 +0000

    build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,13 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
+
 module.exports = merge(common, {
     mode: 'production',
     output: {
+        path: path.resolve(__dirname, 'build'), // Especifica la carpeta de salida
+        filename: '[name].[contenthash].js',
         publicPath: '/'
     },
     plugins: [
